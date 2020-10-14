601af2f512123ba8c96f93b476f6943a0ec72703
hryu0 <49045132+hryu0@users.noreply.github.com>
2020-10-15 08:46:49 +0900

    styling
    intro page and font, layout styles

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
+function responsiveNav() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav-container") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-container";
+    }
+}
